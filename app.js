1dfe9f6b914e5782b0eabd11f099439c0d77d3c3
Artur <Artur.Hoppner@hotmail.com>
2020-09-16 22:49:12 +0200

    added connection to mongoDB

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { request } = require('express');
+// const { request } = require('express');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -19,3 +19,23 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { iseNewUrlParser: true })
+  .then(() => {
+    console.log(`database connection successfully ${db}`);
+  })
+  .catch((err) => {
+    console.log(`unable to connect with database ${err}`);
+  });
+
+app.get('/', (req, res) => {
+  return res.send('<h1>Hello World</h1>');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+let uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@inkognito.oxmlz.mongodb.net/Events?retryWrites=true&w=majority`;
+
+module.exports = {
+  mongoURI: uri,
+};
